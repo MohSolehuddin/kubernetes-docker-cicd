8e06d4eb29813e4fa1bd2f7c3067bf73b0674851
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-06-18 07:38:28 +0700

    init

---
+++ src/main/java/com/mohsolehuddin/kubernetes_docker_cicd/KubernetesDockerCicdApplication.java
@@ -0,0 +1,13 @@
+package com.mohsolehuddin.kubernetes_docker_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDockerCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesDockerCicdApplication.class, args);
+	}
+
+}
